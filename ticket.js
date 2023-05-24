8f2882b522a7a8a5449124127223c385a4d10301
Liliana D´Aquino <ldaq37@gmail.com>
2023-05-24 17:43:47 -0300

    botones
    cambios estéticos en botones

--- ticket.js
+++ ticket.js
@@ -1,3 +1,33 @@
  console.log('Ver JS')
 // // alert(' Sguro???')
 
+// var ticket= 200;
+// var estudiante=80;
+// var trainee= 50;
+// var junior= 15;
+
+//  var cantidad = document.getElmentById("cantidad");
+// // cantidad= prompt(cantidad())
+// var total= cantidad * precio;
+
+// switch (totalFinal)
+// {
+
+//     case estudiante:
+//     total= cantidad * estudiante;
+//     break
+//     case trainee:
+//     total= cantidad * trainee;
+//     break
+//     case junior:
+//     total= cantidad * junior;
+//     break
+//  default:
+//     document.write("Selecione un botón correcto")
+
+
+// }
+
+document.getElementById("borrar").addEventListener("click"())
+
+document.getElementById("formulario").reset();
