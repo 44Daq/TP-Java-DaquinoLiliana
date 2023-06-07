1cb9538f72234dc6ff6d33dfc6e33a0cfeb911c7
Liliana D´Aquino <ldaq37@gmail.com>
2023-06-07 20:42:15 -0300

    centrado-contenedores tickets y oradores

--- ticket.js
+++ ticket.js
@@ -1,49 +1,49 @@
 
 
 // ------------declaración de variables --------------
-var precio= 200;
+const precio= 200;
 var estudiante= 0.80;
 var trainee= 0.50;
 var junior= 0.15;
-var cantidad;
 
 
-
- document.getElmentById("cantidad");
+// const cantidad = document.getElementById("cantidad")
+//  document.addEventListener(prompt("Cuantas entradas quieres"))
+const cantidad = prompt("Cuantas entradas quieres")
 
 //  cantidad= prompt(cantidad())
 
  var totalf= (cantidad * precio);
  console.log((totalf))
 
-//  switch (totalFinal){
-//     case estudiante:
-//       pagar= totalf * estudiante;
-//       break
-//     case trainee:
-//       pagar= totalf * trainee;
-//       break
-//     case junior:
-//      pagar= totalf * junior;
-//      break
+ switch (totalFinal){
+    case estudiante:
+      pagar= totalf * estudiante;
+      break
+    case trainee:
+      pagar= totalf * trainee;
+      break
+    case junior:
+     pagar= totalf * junior;
+     break
 
-//     default: 
-//     document.write("Selecione un botón correcto")
-//     console.log("totalFinal"("click"("resumen")))
-// }
+    default: 
+    document.write("Selecione un botón correcto")
+  console.log("totalf"("click"("resumen")))
+}
 
  
-// var cantidad= document.getElmentById("cantidad");
-// var resumen= document.getElmentById("resumen");
-// var pagar= document.getElmentById("pagar");
 
-// function resumen(){
-//   console.log(categoria.value)
-//   console.log(cantidad.value)
-//   pagar.innerHTML = cantidad.value
-  // document.getElementById("borrar").addEventListener("click"())
-  // document.getElementById("formulario").reset();
-// }
+var resumen= document.getElmentById("resumen");
+var totalFinal= document.getElmentById("pagar");
+
+function resumen(){
+  console.log(categoria.value)
+  console.log(cantidad.value)
+  pagar.innerHTML = cantidad.value
+  document.getElementById("borrar").addEventListener("click"())
+  document.getElementById("formulario").reset();
+}
 
 
 
