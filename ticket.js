6246fd2eababcb242505669ed80a1e448663e5cb
Liliana D´Aquino <ldaq37@gmail.com>
2023-06-30 15:19:36 -0300

    refresh

--- ticket.js
+++ ticket.js
@@ -2,10 +2,18 @@
 
 // ------------declaración de variables --------------
 const precio= 200;
-const categoria =["0.80" ,"0.50","0.15" ]
+const categoria=['estudiante', 'trainee' ,'junior' ]
+estudiante = 0.20;
+trainee= 0.50;
+junior= 0.95;
 
+// const categoria={
+// estudiante : 0.20,
+// trainee : 0.50,
+// junior: 0.95,
 
-var totalf= (cantidad * precio);
+// }
+// var totalf= (cantidad * precio);
 // console.log((totalf))
 
 
@@ -27,12 +35,29 @@ class Compra{
     }
 }
 
-class UI{
-    addCompra(){
+// class UI{
+//     addCompra(){
 
-    }
 
-}
+//     if(element.name ='estudiante'){
+//     alert(nombre +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+//     'Total a pagar' + '$ ' + ((cantidad*200) * estudiante));
+// }
+//     }
+//     else (name ='junior'){
+//         alert(nombre +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+//         'Total a pagar' + '$ ' + ((cantidad*200) * junior));
+//     }
+//     else(name ='traine'){
+//         alert(nombre +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+//         'Total a pagar' + '$ ' + ((cantidad*200) * trainee));
+//     }
+      
+
+// }
+
+
+
 // eventos DOM
 document.getElementById('formulario').addEventListener('submit', function(e){
     const nombre= document.getElementById('nombre'). value;
@@ -40,11 +65,21 @@ document.getElementById('formulario').addEventListener('submit', function(e){
     const categoria= document.getElementById('categoria'). value;
     const cantidad= document.getElementById('cantidad'). value;
 
-    console.log(nombre, apellido, categoria, cantidad, (cantidad*200 * 0.80));
-    const compra= new Compra(nombre, apellido, categoria, cantidad,);
+    // console.log(nombre, apellido, categoria, cantidad, (cantidad*200 * estudiante));
+    // const compra= new Compra(nombre, apellido, categoria, cantidad,);
    e.preventDefault();
 
-   alert(nombre +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +' Total a pagar' + '$ ' + ((cantidad*200) * 0.80 ));
+   alert(nombre + ' ' +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+   'Total a pagar ' + ' $' + ((cantidad*200) * estudiante));
+   document.getElementById("formulario").reset('btn-borrar');
+
+   alert(nombre + ' ' +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+   'Total a pagar ' + ' $' + ((cantidad*200) * trainee));
+   document.getElementById("formulario").reset('btn-borrar');
+
+   alert(nombre + ' ' +  apellido  + ' has seleccionado ' + cantidad + ' entradas  a: ' +  '$200 - ' +  
+   'Total a pagar ' + ' $' + ((cantidad*200) * junior));
+   document.getElementById("formulario").reset('btn-borrar');
  
 
   
