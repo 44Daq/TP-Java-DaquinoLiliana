414088e9e047f398ca1d39adf12e1009b75f5162
Liliana D´Aquino <ldaq37@gmail.com>
2023-07-11 16:12:54 -0300

    V7-js

--- ticket.js
+++ ticket.js
@@ -2,13 +2,13 @@
 // console.log("Liliana");
 
 // ------------declaración de variables --------------
-const PRECIO= 200;
-const TEXTO_PAGAR= "Total a Pagar: $";
-const BOTON_RESUMEN= document.getElementById("resumen");
-// const categoria=['estudiante', 'trainee' ,'junior' ]
-estudiante = 0.20;
-trainee= 0.50;
-junior= 0.95;
+// const PRECIO= 200;
+// const TEXTO_PAGAR= "Total a Pagar: $";
+// const BOTON_RESUMEN= document.getElementById("resumen");
+
+// estudiante = 0.20;
+// trainee= 0.50;
+// junior= 0.95;
 
 // const categoria={
 // estudiante : 0.20,
@@ -90,3 +90,62 @@ junior= 0.95;
 
 // document.getElementById("formulario").reset('btn-borrar');
 
+const PRECIO=200;
+const TEXTO_PAGAR="Total a Pagar: $";
+const BOTON_RESUMEN=document.getElementById("resumen");
+const BOTON_BORRAR=document.getElementById("borrar");
+const INP_NAME=document.getElementById("nombre");
+const INP_SURNAME=document.getElementById("apellido");
+const INP_EMAIL=document.getElementById("correo");
+const INP_NUM=document.getElementById("cantidad");
+var RESULTADO=document.getElementById("pagar");
+var calculo=0;
+var cantidad=0;
+
+
+// estudiante = 0.20;
+// trainee= 0.50;
+// junior= 0.95;
+
+
+BOTON_RESUMEN.addEventListener("click",() => {
+    if(document.getElementById("cantidad").value==""){
+        alert("El valor cantidad no puede estar vacio");
+        return;
+    }else{
+        cantidad=parseInt(document.getElementById("cantidad").value);
+    }
+    if(document.getElementById("categoria").value=="value0"){
+        calculo= PRECIO*80/100;
+        calculo=(PRECIO-calculo)*cantidad;
+        RESULTADO.value=`${TEXTO_PAGAR}${calculo}`;   
+    };
+    if(document.getElementById("categoria").value=="value1"){      
+        calculo= PRECIO*50/100;
+        calculo=(PRECIO-calculo)*cantidad;
+        RESULTADO.value=`${TEXTO_PAGAR}${calculo}`;
+    };
+    if(document.getElementById("categoria").value=="value2"){
+        calculo= PRECIO*15/100;
+        calculo=(PRECIO-calculo)*cantidad;
+        RESULTADO.value=`${TEXTO_PAGAR}${calculo}`;
+    };
+});
+
+BOTON_BORRAR.addEventListener("click",()=>{
+    INP_NAME.value="";
+    INP_SURNAME.value="";
+    INP_EMAIL.value="";
+    INP_NUM.value="";
+
+})
+
+// BOTON_RESUMEN.addEventListener("click",function(){
+//     console.log("Hola sin función arrow!");
+    
+// });
+
+// function Liliana(){
+//     console.log("Hola desde la función!");
+
+// }
