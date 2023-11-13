887dc9b32b8743a2ae90198ff8ed8377bdc5e8db
Liliana D´Aquino <126512554+44Daq@users.noreply.github.com>
2023-11-13 15:57:00 -0300

    v1-3

---
+++ act.js
@@ -0,0 +1,8 @@
+const BOTON_REGISTRO=document.getElementById("botonRegistro");
+
+BOTON_REGISTRO.addEventListener("click",() => {
+   
+        alert("Se ha registrado correctamente");
+        
+        
+    })
