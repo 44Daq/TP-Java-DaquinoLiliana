89ac279b1e36d88a0cc4f4307a797fe3b6780e6f
Liliana D´Aquino <126512554+44Daq@users.noreply.github.com>
2023-11-14 17:15:53 -0300

    login

--- act.js
+++ act.js
@@ -1,8 +1,40 @@
-const BOTON_REGISTRO=document.getElementById("botonRegistro");
+function comprobarClave() {
+    clave1= document.getElementById('clave1');
+    clave2= document.getElementById('clave');
 
-BOTON_REGISTRO.addEventListener("click",() => {
+}
+ 
+    const BOTON_REGISTRO=document.getElementById("botonRegistro");
+
+    BOTON_REGISTRO.addEventListener("click",() => {
+    
+          if (clave1.value == clave2.value) {
+            alert("Se ha registrado correctamente");
+          } else  {
+           alert("La contraseña no coincide");
+          }
+          }
+        
+    )   
+
+  //   function consulta() {
+  //     enviar= document.getElementById('botonEnviar');
+      
+  
+  // }
+  //   const BOTON_ENVIAR=document.getElementById("botonEnviar");
+
+  //   BOTON_ENVIAR.addEventListener("click",() => {
+  //   {
+  //      if("")
+  //      {
+
+  //       alert("Recibimos tu consulta, gracias!");
+  //     } 
+  //   }
+  // }
+  //   )   
+
+
    
-        alert("Se ha registrado correctamente");
-        
-        
-    })
+   
